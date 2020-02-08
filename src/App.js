8e233853a3814757204ad3c5ca900ce0d9d7778a
freeland120 <freeland120@naver.com>
2020-02-09 02:09:37 +0900

    #3 State

--- src/App.js
+++ src/App.js
@@ -1,38 +1,58 @@
 import React from "react";
-
-function TableTennis(props) {
-  return (
-    <div>
-      <h1>I like {props.name}</h1>
-      <img src={props.image}></img>
-    </div>
-  );
-}
+import PropTypes from "prop-types";
 
 const players = [
   {
     id: 1,
     name: "Ma_Long",
     image:
-      "http://www2.pictures.zimbio.com/gi/Ma+Long+Olympics+Day+12+Table+Tennis+WzwGbSgQnoUl.jpg"
+      "http://www2.pictures.zimbio.com/gi/Ma+Long+Olympics+Day+12+Table+Tennis+WzwGbSgQnoUl.jpg",
+    rating: 10.0
   },
   {
     id: 2,
     name: "Fan_Zhendong",
     image:
-      "https://newfanzoneblog.files.wordpress.com/2018/04/fan-zhendong-ittf-world-ranking-table-tennis-e1522773561798.jpg?w=675&h=380&crop=1"
+      "https://newfanzoneblog.files.wordpress.com/2018/04/fan-zhendong-ittf-world-ranking-table-tennis-e1522773561798.jpg?w=675&h=380&crop=1",
+    rating: 9.8
   },
   {
     id: 3,
     name: "Xu_Xin",
     image:
-      "http://www3.pictures.zimbio.com/gi/Xu+Xin+Table+Tennis+Olympics+Day+9+3cnk4fxMlTal.jpg"
+      "http://www3.pictures.zimbio.com/gi/Xu+Xin+Table+Tennis+Olympics+Day+9+3cnk4fxMlTal.jpg",
+    rating: 9.7
   }
 ];
 
-function renderPlayer(who) {
-  return <TableTennis key={who.id} name={who.name} image={who.image} />;
+function TableTennis(props) {
+  return (
+    <div>
+      <h1>
+        I like {props.name}({props.id})
+      </h1>
+      <h4>이 선수의 점수는 :{props.rating}/10.0</h4>
+      <img src={props.image} alt={props.name}></img>
+    </div>
+  );
 }
+
+function renderPlayer(who) {
+  return (
+    <TableTennis
+      key={who.id}
+      name={who.name}
+      image={who.image}
+      rating={who.rating}
+    />
+  );
+}
+
+TableTennis.propTypes = {
+  name: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired
+};
 
 function App() {
   return (
