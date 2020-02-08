e21ac835c9e6387cbcb3d11e636b0b1a5d0e9573
freeland120 <freeland120@naver.com>
2020-02-08 17:18:49 +0900

    JSX & Props

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+function TableTennis(props) {
+  return (
+    <div>
+      <h1>I like {props.name}</h1>
+      <img src={props.image}></img>
+    </div>
+  );
+}
+
+const players = [
+  {
+    id: 1,
+    name: "Ma_Long",
+    image:
+      "http://www2.pictures.zimbio.com/gi/Ma+Long+Olympics+Day+12+Table+Tennis+WzwGbSgQnoUl.jpg"
+  },
+  {
+    id: 2,
+    name: "Fan_Zhendong",
+    image:
+      "https://newfanzoneblog.files.wordpress.com/2018/04/fan-zhendong-ittf-world-ranking-table-tennis-e1522773561798.jpg?w=675&h=380&crop=1"
+  },
+  {
+    id: 3,
+    name: "Xu_Xin",
+    image:
+      "http://www3.pictures.zimbio.com/gi/Xu+Xin+Table+Tennis+Olympics+Day+9+3cnk4fxMlTal.jpg"
+  }
+];
+
+function renderPlayer(who) {
+  return <TableTennis key={who.id} name={who.name} image={who.image} />;
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Which player is best in table tennis?</h1>
+      {players.map(renderPlayer)}
     </div>
   );
 }
